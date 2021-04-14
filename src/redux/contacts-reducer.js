032e9bf2c792fe01cd7e1a5bceb11e490bbee6b6
Oleh Sydorenko <oleh.sydorenko1994@gmail.com>
2021-04-14 22:47:32 +0300

    fix 1

--- src/redux/contacts-reducer.js
+++ src/redux/contacts-reducer.js
@@ -5,8 +5,9 @@ import action from './contacts-actions';
 const contacts = createReducer([], {
   [action.fetchContactsSuccsess]: (_, { payload }) => payload,
   [action.addContactsSuccsess]: (state, { payload }) => [...state, payload],
-  [action.deleteContactSuccsess]: (state, { payload }) =>
-    state.filter(({ id }) => id !== payload),
+  [action.deleteContactsSuccsess]: (state, { payload }) => {
+    return state.filter(({ id }) => id !== payload);
+  },
 });
 
 const filter = createReducer('', {
